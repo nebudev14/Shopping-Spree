c14ece6f1c00ce3e6179f42b752bcc622169b2a6
warren yun <xxwarrenhdxx@gmail.com>
2021-07-24 09:59:11 -0400

    Start styling index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,39 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div>
 
+  const redirectSearch = async (event: any) => {
+    event.preventDefault();
+
+    console.log(event.target.name.value);
+  }
+
+
+  return (
+    <div className="container">
+      <form onSubmit={redirectSearch} className="formContainer">
+        <label htmlFor="name" className="title">What are you looking for?</label>
+        <input id="name" name="name" type="text" autoComplete="name" className="textBox" required />
+        <button type="submit" className="btn">Search</button>
+      </form>
+
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-direction: column; 
+        }
+
+        .formContainer {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-direction: column; 
+        }
+        
+      `}</style>
     </div>
   )
 }
