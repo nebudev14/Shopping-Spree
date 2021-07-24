708bc16d1a0e2167142ea140c7086cd261a5e14f
warren yun <xxwarrenhdxx@gmail.com>
2021-07-23 22:25:41 -0400

    Basic structure for item thumbnail component

--- components/ItemThumbnail.tsx
+++ components/ItemThumbnail.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-function ItemThumbnail() {
+import Link from 'next/link';
+
+
+export default function ItemThumbnail({item}: any) {
     return (
-        <div>
-            
-        </div>
+        <Link href={`/item/${item.product_id}`}>
+            <div className="itemThumbnail">
+                <img src={item.thumbnail} className="itemImage"/>
+                <span className="title">{item.title}</span>
+                <span className="price">{item.price}</span>
+                <style jsx>{`
+                    .title {
+                        color: red;
+                    }
+
+                `}</style>
+            </div>
+        </Link>
     );
 }
-
-export default ItemThumbnail;
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react';
+
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
