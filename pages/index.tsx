81ee50c79807c4de00b1cfcf81c153cee451f7a5
warren yun <xxwarrenhdxx@gmail.com>
2021-07-24 10:12:37 -0400

    Finish styling index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     <div className="container">
       <form onSubmit={redirectSearch} className="formContainer">
         <label htmlFor="name" className="title">What are you looking for?</label>
-        <input id="name" name="name" type="text" autoComplete="name" className="textBox" required />
+        <input id="name" name="name" type="text" autoComplete="off" className="textBox" required />
         <button type="submit" className="btn">Search</button>
       </form>
 
@@ -35,7 +35,39 @@ export default function Home() {
           justify-content: center;
           flex-direction: column; 
         }
-        
+
+        .title {
+          font-size: 250%;
+          margin: 2rem 0 2rem 0;
+        }
+
+        .textBox {
+          width: 70%;
+          height: 2rem;
+          margin: 2rem 0 2rem 0;
+          border-radius: 20px;
+          outline: 0;
+        }
+
+        .btn {
+          padding:10px;
+          border:none;
+          background-color: var(--primary-color);
+          color: #fff;
+          font-weight: 400;
+          border-radius: 20px;
+          margin: 5px;
+          width:25%;
+          align-items: center;
+          transition-duration: 0.4s;    
+        }
+
+        .btn:hover {
+          box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+          transform: scale(1.02);
+          cursor: pointer;
+        }
+
       `}</style>
     </div>
   )
