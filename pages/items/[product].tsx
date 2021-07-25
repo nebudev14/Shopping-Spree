cbaae9fb0001262ab88bfb05e9d14b371c255b26
warren yun <xxwarrenhdxx@gmail.com>
2021-07-24 23:55:12 -0400

    Get server sidef props for product information page

--- pages/items/[product].tsx
+++ pages/items/[product].tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 export default function productResult(props: any) {
     return  (
@@ -6,4 +7,26 @@ export default function productResult(props: any) {
             <h1>i am a product result</h1>
         </div>
     );
+}
+
+export const getServerSideProps: any = async (context: any) => {
+    var information;
+    const {productID} = context.params;
+    var body = {
+        "id": productID
+    };
+    await (async () => {
+        const itemInfo = await axios({
+            method: 'GET',
+            url: 'http://localhost:3000/api/productList',
+            data: body
+        });
+        information = itemInfo.data.message;
+    })();
+
+    return {
+        props: {
+            information
+        }
+    }
 }
