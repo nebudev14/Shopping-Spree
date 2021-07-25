3682aa73071a620d0b4bc15e05da776dfc825e95
warren yun <xxwarrenhdxx@gmail.com>
2021-07-25 08:20:02 -0400

    Create map component

---
+++ components/Map.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { GoogleMap, Marker } from "react-google-maps";
+
+const Map: any = ({xCoord}: any, {yCoord}: any) => { 
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Map;
+
