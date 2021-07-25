0da48e28866649249ba24a6bf615a2f3d81ddc69
warren yun <xxwarrenhdxx@gmail.com>
2021-07-25 08:29:15 -0400

    Finish map component

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { GoogleMap, Marker } from "react-google-maps";
 
-const Map: any = ({xCoord}: any, {yCoord}: any) => { 
+const Map: any = ({lat}: any, {lng}: any) => { 
     return (
-        <div>
-
-        </div>
+        <GoogleMap
+            center= {{ lat: lat, lng: lng }}
+            zoom={10}
+        >
+            <Marker
+                key={1}
+                position={{lat: lat, lng: lng}}
+            />
+        </GoogleMap>    
     );
 }
 
