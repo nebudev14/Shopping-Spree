098742f2be8eb529c15e13c88f2f13e4df64cc7c
warren yun <xxwarrenhdxx@gmail.com>
2021-07-24 23:21:26 -0400

    Style results

--- components/ItemThumbnail.tsx
+++ components/ItemThumbnail.tsx
@@ -4,14 +4,57 @@ import Link from 'next/link';
 
 const ItemThumbnail: any = ({item}: any) => {
     return (
+        
         <Link href={`/items/${item.product_id}`}>
             <div className="itemThumbnail">
                 <img src={item.thumbnail} className="itemImage"/>
-                <span className="title">{item.title}</span>
-                <span className="price">{item.price}</span>
+                <div className="itemInfo">
+                    <span className="title">{item.title}</span>
+                    <span className="price"><strong>Price:</strong> {item.price}</span>
+                </div>
                 <style jsx>{`
-                    .itemThumbnail {
 
+                    .container {
+                        display: inline;
+                    }
+
+                    .itemThumbnail {
+                        border: 5px solid #000;
+                        border-radius: 20px;
+                        text-align: center;
+                        margin-top: 2rem;
+                        margin-bottom: 2rem;
+                        display: flex;
+                        transition: 200ms ease;
+                    }
+
+                    .itemThumbnail:hover {
+                        cursor: pointer;
+                        box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+                        transform: scale(1.02);
+                    }
+                    
+                    .itemInfo {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        flex-direction: column;
+                    }
+
+                    .itemImage {
+                        width: 300px;
+                        height: 300px;
+                        border-radius: 20px;
+                    }
+
+                    .title {
+                        font-size: 120%;
+                        margin-bottom: 2rem;
+                        width: 75%;
+                    }
+
+                    .price {
+                        font-size: 110%;
                     }
                 `}</style>
             </div>
--- pages/results/[productResult].tsx
+++ pages/results/[productResult].tsx
@@ -7,36 +7,37 @@ export default function Product(props: any) {
     return (
         <div className="container">
             <span className="title">
-                Results for {props.product}
+                Results for {props.productResult}
             </span>
-            <div className="itemsDiv">
+            <div className="itemDiv">
                 {props.results.map((item: any, i: any) => (
-                    <ItemThumbnail key={i} item={item} />
+                    <ItemThumbnail key={i} item={item} className="item"/>
                 ))}
             </div>
 
             <style jsx>{`
 
                 .container {
-                    display: flex;
-                    align-items: center;
-                    justify-content: start;
-                    flex-direction: column;
-                    height: 100vh;
-                    margin-top: 3rem;
                     text-align: center;
+                    padding: 1.5rem;
                 }
-
+                
                 .title {
                     font-size: 400%;
                     color: var(--primary-color);
                     border-bottom: 2px solid var(--primary-color);
-                    margin-top: 3rem;
-                }   
+                    margin: 2.5rem 0 2.5rem 0;
+                }
 
-                .itemsDiv {
-                    text-align: center;
-                    margin: 3rem;
+                .item {
+                    margin-top: 3rem;
+                    margin-bottom: 3rem;
+                }
+
+                .itemDiv {
+                    width: 900px;
+                    display: grid;
+                    margin: auto;
                 }
 
             `}</style>
